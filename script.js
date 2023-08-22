de8491b0e6e765eb3b82115d91c1f3aabce07184
Jaime Sanchez <118962371+Jaime-Sanz@users.noreply.github.com>
2023-08-21 22:07:39 -0700

    fin

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 let number1 = document.getElementById("display-1");
 let number2 = document.getElementById("display-2");
 let operator = document.getElementById("display-x");
+let result = document.getElementById("result");
 let x = 0;
 let y = 0;
+let displayValue = 0;
 
 
 function getNumbers(numbers){
@@ -28,41 +30,50 @@ function valueConversion(){
             addition(x, y);
         break;
         case "-":
-            subtraction(x,y);
+            subtraction(x, y);
         break;
         case "/":
-            division(x,y);
+            division(x, y);
+        break;
         case "x":
-            multiplication(x,y);
+            multiplication(x, y);
         break;
         case "%":
-            percentage(x,y);
+            percentage(x, y);
         break;
     }
 }
 
 function addition(num1, num2) {
-    let displayValue = num1 + num2;
+    displayValue = Math.round((num1 + num2)* 1000) / 1000;;
     result.innerText = displayValue;
 }
 
-function subtraction(){
-    let displayValue = num1 - num2;
+function subtraction(num1, num2){
+    displayValue = Math.round((num1 - num2)* 1000) / 1000;;
     result.innerText = displayValue;
 }
 
-function division(){
-    let displayValue = num1 / num2;
+function division(num1, num2){
+    if(num2 === 0){
+        number1.innerText = ""
+        operator.innerText = ""
+        number2.innerText = ""
+        result.innerText = "You Know Better!";
+        result.style.fontSize = "22px";
+    } else {
+        displayValue = Math.round((num1 / num2)* 1000) / 1000;
+        result.innerText = displayValue;
+    }
+}
+
+function multiplication(num1, num2){
+    displayValue = Math.round((num1 * num2)* 1000) / 1000;
     result.innerText = displayValue;
 }
 
-function multiplication(){
-    let displayValue = num1 * num2;
-    result.innerText = displayValue;
-}
-
-function percentage(){
-    let displayValue = (x/100)*y;
+function percentage(num1, num2){
+    displayValue = displayValue = Math.round(((x/100)*y)* 1000) / 1000;
     result.innerText = displayValue;
 }
 
@@ -71,12 +82,23 @@ function eraseEverything(){
     number2.innerText = ""
     operator.innerText = ""
     result.innerText = ""
+    if(result.style.fontSize === "22px"){
+        result.style.fontSize = "48px";
+    }
 }
 
 function eraseOne(){
-    if(number2.innerText === ""){
+    if (number1.innerText === "" && number2.innerText === "" && operator.innerText === ""){
+        result.innerText = result.innerText.substr(0,result.innerText.length-1);
+    } else if (operator.innerText != "" && number2.innerText === ""){
+        operator.innerText = "";
+    } else if(number2.innerText === ""){
         number1.innerText = number1.innerText.substr(0,number1.innerText.length-1);
-    } else if (number1.innerText != ""){;
+    } else if (number1.innerText != ""){
         number2.innerText = number2.innerText.substr(0,number2.innerText.length-1);
     }
+}
+
+const gitRead = () => {
+    window.open("https://github.com/Jaime-Sanz/Calculator")
 }
